d475e1ef0e52fef0c50ef2f02330ab2bc227b47a
James Spencer <142933516+RifleCityCoding@users.noreply.github.com>
2023-09-08 16:24:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
 
 /* Here we are creating the variables */
 
-const storyText = 'It was :insertTemp: fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+const storyText = 'It was :insertTemp: fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. John Doe saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
