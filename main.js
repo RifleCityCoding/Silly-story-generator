b7e9c4b614b3f525f243108d8042cab8964ed7f0
James Spencer <142933516+RifleCityCoding@users.noreply.github.com>
2023-09-08 16:43:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((insertTemp - 32) * 5 / 9)} centigrade`;
-    newStory= newStory.replaceAll(fahrenheit, temperature);
+    newStory= newStory.replaceAll('fahrenheit', temperature);
     newStory = newStory.replaceAll('300 pounds', weight);
   }
 
